48d59774a4a091ced97b0b764ae08d116b7ec50d
Your Name <your.email@example.com>
2025-02-23 23:03:58 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -24,3 +24,27 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails());
 // updating the stock, subtracting 3
+
+// Task 2: Creating an Order Class
+class Order {
+constructor(orderId, product, quantity) {
+    this.orderId = orderId;
+    this.product = product;
+    this.quantity = quantity; 
+// creating an order class with details
+product.updateStock(quantity); //updates stock as each order is made
+}
+
+getOrderDetails() {
+    return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`;
+   } // returns order details 
+}
+
+// test cases:
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+//update stock and log product
+console.log(prod1.getDetails());
+
+
+//
